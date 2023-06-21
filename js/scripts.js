c9ccad9124fb7a234bafa9a861bbf42d008b1254
gbayarri <genis.bayarri@irbbarcelona.org>
2023-06-21 12:26:45 +0200

    Added secondary release buttons and fixed styles

--- js/scripts.js
+++ js/scripts.js
@@ -58,16 +58,16 @@ window.addEventListener('DOMContentLoaded', async(event) => {
 
     let latest_version
     // SW version (TODO WHEN PUBLIC REPO)
-    /*fetch('https://raw.githubusercontent.com/gbayarri/biomovies-src/master/package.json')
+    fetch('https://raw.githubusercontent.com/gbayarri/biomovies-src/master/package.json')
     .then(res => res.json())
     .then(json => {
         latest_version = json.version
         document.querySelectorAll(".version").forEach(item => item.innerHTML = json.version)
         document.querySelectorAll(".versionName").forEach(item => item.innerHTML = json.versionName)
-    })*/
+    })
 
     // Releases (TODO WHEN PUBLIC REPO)
-    /*fetch('https://api.github.com/repos/gbayarri/biomovies-src/releases')
+    fetch('https://api.github.com/repos/gbayarri/biomovies-src/releases')
     .then(res => res.json())
     .then(json => {
 
@@ -93,23 +93,49 @@ window.addEventListener('DOMContentLoaded', async(event) => {
             }
         })
 
-        // get list of releases
-        const releases_list = json.map(item => ({name: item.name, tag: item.tag_name, published_at: item.published_at, url: item.html_url}))
         const selectReleases = document.getElementById("select-releases")
-        // fill select
-        releases_list.forEach(item => {
+        // fill select with release name and date
+        json.forEach(item => {
             const d = new Date(item.published_at)
             const publish_date = `${(d.getDate() < 10 ? '0' : '') + d.getDate()}/${(d.getMonth() < 9 ? '0' : '') + (parseInt(d.getMonth()) + 1)}/${d.getFullYear()}`
-            selectReleases.options[selectReleases.options.length] = new Option(`${item.name} - ${publish_date}`, item.url);
+            //selectReleases.options[selectReleases.options.length] = new Option(`${item.name} - ${publish_date}`, item.url);
+            selectReleases.options[selectReleases.options.length] = new Option(`${item.name} - ${publish_date}`, item.tag_name);
         })
         // add window action to each option
         selectReleases.addEventListener("change", (e) => {
-            if(selectReleases.value) window.open(selectReleases.value);
+            // disable all secondary buttons
+            document.getElementById("arm64-dmg-sec").classList.add("disabled")
+            document.getElementById("intel-dmg-sec").classList.add("disabled")
+            document.getElementById("exe-sec").classList.add("disabled")
+            document.getElementById("amd64-deb-sec").classList.add("disabled")
+            if(selectReleases.value) {
+                // show secondary buttons and enable / add links if necessary
+                document.getElementById("sec-assets").style.display = "flex"
+                var sec_assets = json.filter(item => item.tag_name === selectReleases.value)[0].assets
+                sec_assets.forEach(item => {
+                    if(item.name.indexOf('dmg') !== -1 && item.name.indexOf('arm64') !== -1) {
+                        document.getElementById("arm64-dmg-sec").classList.remove("disabled")
+                        document.getElementById("arm64-dmg-sec").setAttribute('href', item.browser_download_url);
+                    }
+                    if(item.name.indexOf('dmg') !== -1 && item.name.indexOf('arm64') === -1) {
+                        document.getElementById("intel-dmg-sec").classList.remove("disabled")
+                        document.getElementById("intel-dmg-sec").setAttribute('href', item.browser_download_url);
+                    }
+                    if(item.name.indexOf('exe') !== -1) {
+                        document.getElementById("exe-sec").classList.remove("disabled")
+                        document.getElementById("exe-sec").setAttribute('href', item.browser_download_url);
+                    }
+                    if(item.name.indexOf('deb') !== -1 && item.name.indexOf('amd64') !== -1) {
+                        document.getElementById("amd64-deb-sec").classList.remove("disabled")
+                        document.getElementById("amd64-deb-sec").setAttribute('href', item.browser_download_url);
+                    }
+                })
+            } else document.getElementById("sec-assets").style.display = "none"
         }, false);
         // enable select
         selectReleases.disabled = false
 
-    })*/
+    })
 
     // bac to top
     let mybutton = document.getElementById("btn-back-to-top");
